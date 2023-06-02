a019abba70893f30eb914857a668f043dea14531
DiegoCamilli <camillidiego@yahoo.com>
2023-06-02 12:38:56 -0700

    added init function

--- index.js
+++ index.js
@@ -127,7 +127,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
